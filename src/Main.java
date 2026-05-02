223a6b88dd2ac7bec7ff7d254fea6269c5959482
Erika Franzelle Bingco <franzellebingco@gmail.com>
2026-05-03 02:10:55 +0800

    feat: add Intro1 class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import Storyline.*;
+import Storyline.World1.*;
 import Storyline.World2.*;
 
 public class Main {
     public static void main(String[] args){
         Opening.opening();
+        Intro1.intro1();
         Intro2.intro2();
     }
 }
