99586b6ee1b2d432286eea67e13ca27bcf890ff1
Alexandra <alexandralouie.mauring@cit.edu>
2026-05-07 16:49:12 +0800

    feat: add CharacterBuilder class

--- src/Core/CharacterBuilder.java
+++ src/Core/CharacterBuilder.java
@@ -1,4 +1,18 @@
 package Core;
 
+import Races.Kapre;
+import Races.Tikbalang;
+
 public class CharacterBuilder {
+    public static PlayerCharacter build(String name, int raceChoice) {
+        Race race = null;
+
+        if (raceChoice == 1) {
+            race = new Tikbalang();
+        } else if (raceChoice == 2) {
+            race = new Kapre();
+        }
+
+        return new PlayerCharacter(name, race);
+    }
 }
