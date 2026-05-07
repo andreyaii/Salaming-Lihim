56abcc1542e7acb9283517fd9e3f3726b43dd5b5
Alexandra <alexandralouie.mauring@cit.edu>
2026-05-07 16:58:53 +0800

    feat: add InventoryMenu class, no game logic implemented yet

--- src/Core/InventoryMenu.java
+++ src/Core/InventoryMenu.java
@@ -1,4 +1,19 @@
 package Core;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class InventoryMenu {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void open(PlayerCharacter player) {
+        List<Item> inv = player.getInventory();
+
+        if (inv.isEmpty()) {
+            System.out.print("                                                   ┏━───────────────────────────────────────────━┓\n");
+            System.out.print("                                                            ++ Your inventory is empty. ++\n");
+            System.out.println("                                                   ┗━───────────────────────────────────────────━┛");
+            return;
+        }
+    }
 }
