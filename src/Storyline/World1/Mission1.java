ccf57920a3f0ad3981278d62472d3f3768b9f968
Erika Franzelle Bingco <franzellebingco@gmail.com>
2026-05-08 18:32:08 +0800

    feat: update World1: Mission1 dialogue

--- src/Storyline/World1/Mission1.java
+++ src/Storyline/World1/Mission1.java
@@ -25,5 +25,23 @@ public class Mission1 {
         System.out.println("[You encountered 3 duwendes. Choose a target and an attack]");
         System.out.println();
         DialogueUtils.pause();
+
+        //fight not implemented yet
+
+        System.out.println("\n\nSean: Thank you so much. I owe you my livelihood. Tell me, what would you like in return?");
+        scan.nextLine();
+
+        System.out.print(player.name + ": I’m actually investigating a bunch of disappearances around the city. Do you know anything about that?\n");
+        scan.nextLine();
+
+        System.out.println("Sean: Well, the word on the street is that some sort of giant is kidnapping Enkantos and Diwatas. If you want to know more, you might want to\n" +
+                "       head to the Information Guild. My name is Sean. Tell them I sent you.");
+        scan.nextLine();
+
+        System.out.println("You agree and go on your way.");
+        System.out.println("After a while, you see a cozy bench and decide to take a rest.");
+        System.out.println();
+        player.rest();
+        DialogueUtils.pause();
     }
 }
