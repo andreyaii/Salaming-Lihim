c4cb23f144f0826194bf1cd008e0b41ac35c4e46
Andrea <dreyalabajos@gmail.com>
2026-05-09 08:51:16 +0800

    feat: add spawn boss method

--- src/Core/EnemyFactory.java
+++ src/Core/EnemyFactory.java
@@ -6,15 +6,25 @@ import java.util.Random;
 
 public class EnemyFactory {
 
+    //spawn normal enemies
     public static List<Enemy> spawnEnemies(EnemyType type, int count) {
         List<Enemy> enemies = new ArrayList<>();
         for (int i = 1; i <= count; i++) {
             String name = type.typeName + " " + i;
-            int hp = 50 + new Random().nextInt(17);
+            int hp = 50 + new Random().nextInt(17); //55-67 hp
             enemies.add(new Enemy(name, type, hp));
         }
         return enemies;
     }
 
-    //boss
+    //spawn bosses
+    public static List<Enemy> spawnBosses(EnemyType type, int count) {
+        List<Enemy> bosses = new ArrayList<>();
+        for (int i = 1; i <= count; i++) {
+            String name = "Boss " + type.typeName + " " + i;
+            int hp = 100 + new Random().nextInt(50); // 100–150 HP
+            bosses.add(new Enemy(name, type, hp));
+        }
+        return bosses;
+    }
 }
